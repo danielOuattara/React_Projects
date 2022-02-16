1365058ded20af0f8d3aa55ddb68206bbfcd521d
daniel ouattara <daniel.ouattara@gmx.com>
2022-02-16 11:03:00 +0100

    Completed Lorem Ipsum Generator: OK

--- 08-lorem-ipsum/my_solution/src/App.js
+++ 08-lorem-ipsum/my_solution/src/App.js
@@ -3,40 +3,32 @@ import data from './data';
 
 
 function App() {
-  
   const [ numberOfParagraph, setNumberOfParagraph ] = useState(0);
-
   const [ text, setText ] = useState([]);
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
-    setText(data)
-  }
-
-  let result = "";
-   for (let i = 0; i < numberOfParagraph; i++) {
-     result += data[i]
-  }
-  
+      setText(() => data.slice(0, numberOfParagraph));
+    }
+    
   return (
       <section className='section-center'>
         <h3>tried of boring lorem ipsum ?</h3>
 
         <form className='lorem-form' onSubmit={handleSubmit}>
-
           <label htmlFor="numbParagraph">paragraph ?</label>
-
           <input 
+            id="numbParagraph"
             type="number" 
+            min="1"
+            max={data.length}
             value={numberOfParagraph}
             onChange={(event) => setNumberOfParagraph(event.target.value)} 
           />
-          
-          <button 
-            type="submit" 
-            className='btn'>Fetch</button>
 
+             <button type="submit" className='btn'>Fetch</button> 
         </form>
+
         <article className="lorem-text">
           {text.map((item, index ) => {
             return(
