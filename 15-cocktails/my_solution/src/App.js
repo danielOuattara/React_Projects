12ad22e0c471f7ee096b9a0ebe84ca31a965b393
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-11 23:36:56 +0200

    switching to react router 6

--- 15-cocktails/my_solution/src/App.js
+++ 15-cocktails/my_solution/src/App.js
@@ -1,5 +1,35 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// // import pages
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import SingleCocktail from "./pages/SingleCocktail";
+// import Error from "./pages/Error";
+
+// // import components
+// import Navbar from "./components/Navbar";
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Switch>
+//         <Route exact path="/"><Home /></Route>
+//         <Route path="/about"><About /></Route>
+//         <Route path="/coktail/:id"><SingleCocktail /> </Route>
+//         <Route path="*"><Error /></Route>
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+/* To react router 6 
+-----------------------*/
+
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // import pages
 import Home from "./pages/Home";
@@ -13,12 +43,12 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route path="/about"><About /></Route>
-        <Route path="/coktail/:id"><SingleCocktail /> </Route>
-        <Route path="*"><Error /></Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/coktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </Router>
   );
 }
