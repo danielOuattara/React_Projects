894d0e50cd51d6de24a9d8dfceca01fb6ec8d8c7
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-11 16:45:22 +0200

    About and Error page set up done

--- 15-cocktails/my_solution/src/pages/Error.js
+++ 15-cocktails/my_solution/src/pages/Error.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  )
-}
+    <section className="error-page section">
+      <div className="error-container">
+        <h2>Ooops ! Is's a dead end</h2>
+        <Link to="/" className="btn btn-primary"> back to Home</Link>
+      </div>
+    </section>
+  );
+};
 
-export default Error
+export default Error;
