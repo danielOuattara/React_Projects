e77c4e4b22426c896d1ba46d8919417d41d6a764
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-11 18:28:50 +0200

    cocktails context API Set up

--- 15-cocktails/my_solution/src/components/CocktailList.js
+++ 15-cocktails/my_solution/src/components/CocktailList.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if(cocktails.length === 0) {
+    return  <h2 className="section-title"> No cocktail match your search </h2>
+  };
+
   return (
     <div>
       <h2>cocktail list component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default CocktailList
+export default CocktailList;
--- 15-cocktails/my_solution/src/components/SearchForm.js
+++ 15-cocktails/my_solution/src/components/SearchForm.js
@@ -1,12 +1,9 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+  const { setSearchTerm } = useGlobalContext();
+  return <div></div>;
+};
 
-export default SearchForm
+export default SearchForm;
