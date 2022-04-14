a127ec6b9f4e20670614aca64131a7fa70baf4ff
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-14 15:51:21 +0200

    markdown project, completed

---
+++ 16-markdown-preview/my_solution/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+
+function App() {
+  const [markdown, setMarkdown] = useState("# markdown previews");
+
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(event) => setMarkdown(event.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default App;
+
