d0aa92309592e5eb0fd5af5acda9542b383d525f
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-19 11:46:50 +0200

    API call completed, Next Optimizing Icons display?

--- 17-random-person/my_solution/src/App.js
+++ 17-random-person/my_solution/src/App.js
@@ -11,14 +11,60 @@ const url = "https://randomuser.me/api/";
 const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [person, setPerson] = useState(null);
   const [title, setTitle] = useState("name");
   const [value, setValue] = useState("random person");
 
   const handleValue = (event) => {
-    console.log(event.target);
+    if (event.target.classList.contains("icon")) {
+      // setTitle(event.target.attributes[1].nodeValue)
+      // setValue(person[event.target.attributes[1].nodeValue])
+
+      setTitle(event.target.dataset.label);
+      setValue(person[event.target.dataset.label]);
+    }
   };
+
+  const fetchUser = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw Error("Something went wrong");
+      }
+
+      const data = await response.json();
+      const person = data.results[0];
+      const { first, last } = person.name;
+      const { age } = person.dob;
+      const { email, phone } = person;
+      const { password } = person.login;
+      const { large: image } = person.picture;
+      const { number, name } = person.location.street;
+
+      const newPerson = {
+        name: `${first} ${last}`,
+        age,
+        image,
+        email,
+        street: `${number} ${name}`,
+        phone,
+        password,
+      };
+
+      setPerson(newPerson);
+      setValue(newPerson.name);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
     <main>
@@ -30,7 +76,7 @@ function App() {
             alt="random user"
             className="user-img"
           />
-          <p className="user-title">my {title} is </p>
+          <p className="user-title">Hi, My {title} is</p>
           <p className="user-value">{value}</p>
           <div className="values-list">
             <button
@@ -77,7 +123,7 @@ function App() {
               <FaLock />
             </button>
           </div>
-          <button className="btn" type="button">
+          <button className="btn" type="button" onClick={fetchUser}>
             {loading ? "loading..." : "random user"}
           </button>
         </div>
