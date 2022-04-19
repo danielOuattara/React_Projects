be3213d2bd475e652acc0c6feb6c7b6d29bd4c92
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-19 12:39:17 +0200

    Pagination project, basic setup & API requests

---
+++ 18-pagination/my_solution/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+function App() {
+  const { loading, data } = useFetch();
+  return (
+    <main>
+      <div className="section-title">
+        <h1> {loading ? "loading..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((person) => {
+            return <Follower key={person.id}{...person} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
