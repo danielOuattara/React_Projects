bb6165e9ebe39f40bc0f6b58ca3e6735e84dd1fa
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-01 19:55:19 +0200

    start project initial fetch, next infinite scroll

--- 19-stock-photos/my_solution/src/App.js
+++ 19-stock-photos/my_solution/src/App.js
@@ -1,12 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>;
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    try {
+      setLoading(true);
+      let url;
+      url = `${mainUrl}${clientID}`;
+      const response = await fetch(url);
+      if(!response.ok) {
+        throw new Error (`${response.statusText} ${response.status} `)
+      }
+      const data = await response.json();
+
+      setPhotos(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error.message)
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder="search" className="form-input" />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">Loading...</h2>}
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 19-stock-photos/my_solution/src/Photo.js
+++ 19-stock-photos/my_solution/src/Photo.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = (props) => {
+  const {
+    urls: { regular },
+    alt_description,
+    likes,
+    user: {
+      name,
+      portfolio_url,
+      profile_image: { medium },
+    },
+  } = props;
+  return (
+    <article className="photo">
+      <img src={regular} alt={alt_description} />
+      <div className="photo-info">
+        <div>
+          <h4>{name}</h4>
+          <p>{likes}</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} className="user-img" />
+        </a>
+      </div>
+    </article>
+  );
+};
 
-export default Photo
+export default Photo;
