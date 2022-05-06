936e2f94bbea2df230323b925fe48275bab00cdf
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-06 21:55:58 +0200

    fetch & infinite fetches Ok, next search query

--- 19-stock-photos/my_solution/src/App.js
+++ 19-stock-photos/my_solution/src/App.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
+
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
-const searchUrl = `https://api.unsplash.com/search/photos/`;
+// const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  console.log("page = ", page);
 
   const fetchImages = async () => {
     try {
       setLoading(true);
+      let nextPages = `&page=${page}`;
       let url;
-      url = `${mainUrl}${clientID}`;
+      url = `${mainUrl}${clientID}${nextPages}`;
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`${response.statusText} ${response.status} `);
       }
       const data = await response.json();
-
-      setPhotos(data);
+      setPhotos((previousData) => [...previousData, ...data]);
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -30,29 +33,31 @@ function App() {
 
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     const event = window.addEventListener("scroll", () => {
       // console.log(`inner height ${window.innerHeight}`);
       // console.log(`scroll_Y ${window.scrollY}`);
       // console.log(`body_height ${window.document.body.scrollHeight}`);
-      const margin = 400;
+      const margin = 40;
       if (
-       !loading && window.innerHeight + window.scrollY + margin >=
-        window.document.body.scrollHeight
-        ) {
-        setLoading(true)
-        console.log("===>  Waiting for Next Fetch");
+        !loading &&
+        window.innerHeight + window.scrollY + margin >=
+          window.document.body.scrollHeight
+      ) {
+        setLoading(true);
+        setPage((page) => page + 1);
       }
     });
-
     return () => window.removeEventListener("scroll", event);
   }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log("Hello");
   };
+
   return (
     <main>
       <section className="search">
@@ -65,8 +70,8 @@ function App() {
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((photo) => {
-            return <Photo key={photo.id} {...photo} />;
+          {photos.map((photo, index) => {
+            return <Photo key={index} {...photo} />;
           })}
         </div>
         {loading && <h2 className="loading">Loading...</h2>}
