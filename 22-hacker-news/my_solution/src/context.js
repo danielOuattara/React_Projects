835ed45ac366ac1de348166d9d9f576692f50a89
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-10 20:31:04 +0200

    hacker news API, fetch stories OK, next display

---
+++ 22-hacker-news/my_solution/src/context.js
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect, useReducer } from "react";
+
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions";
+import reducer from "./reducer";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
+
+const initialState = {
+  isLoading: false,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
+  error: null,
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`${response.statusText} ${response.status} `);
+      }
+      const data = await response.json();
+      // console.log("data = ", data);
+      // console.log("before dispatch hits");
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+  }, []);
+
+  console.log("state =", state);
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ 22-hacker-news/my_solution/src/reducer.js
@@ -0,0 +1,28 @@
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true };
+
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: [...state.hits, action.payload.hits],
+        nbPages: action.payload.nbPages,
+      };
+
+    // default:
+    //   return state;
+    default:
+      throw new Error(`No matching action type ${action.type} `);
+  }
+};
+export default reducer;
