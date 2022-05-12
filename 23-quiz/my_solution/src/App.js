e5159a333372e8fd940cc6c8e86ff9539db9d220
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-12 17:06:14 +0200

    Axios API, questions, Next question OK

--- 23-quiz/my_solution/src/App.js
+++ 23-quiz/my_solution/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useGlobalContext } from "./context";
-
 import SetupForm from "./SetupForm";
 import Loading from "./Loading";
 import Modal from "./Modal";
+
 function App() {
   const { waiting, loading, questions, index, correctAnswers } =
     useGlobalContext();
@@ -14,9 +14,35 @@ function App() {
   if (loading) {
     return <Loading />;
   }
-  return <main>
-    quiz app
-  </main>;
+  const { question, incorrect_answers, correct_answer } = questions[0];
+  console.log(questions[0]);
+  const answers = [...incorrect_answers, correct_answer];
+  console.log("answers =", answers);
+  return (
+    <main>
+      <Modal />
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers: {correctAnswers} / {index}
+        </p>
+        <article className="container">
+          <h3 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  dangerouslySetInnerHTML={{ __html: item }}
+                  className="answer-btn"
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">Next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
