f0c23c84422647f39a28bbad23025964a02ce7d0
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-31 11:39:20 +0200

    starting GithubContext + GitHubProvider

--- 24_Search_Github_Users/my_solution/src/components/Info.js
+++ 24_Search_Github_Users/my_solution/src/components/Info.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { GoRepo, GoGist } from 'react-icons/go';
 import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+  const data = useContext(GithubContext)
+  
+  return <h2>{data}</h2>;
 };
 
 const Wrapper = styled.section`
