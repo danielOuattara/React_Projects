11e7f0bfb545968568deb392d94a8effb6bb90d8
danielOuattara <daniel.ouattara@gmx.com>
2022-10-31 11:32:19 +0100

    revisited + optimized

--- 08-lorem-ipsum/my_solution/src/functions_solution/AppFunction.js
+++ 08-lorem-ipsum/my_solution/src/functions_solution/AppFunction.js
@@ -1,41 +1,40 @@
-import React, { useState } from 'react';
-import data from './../data';
-
+import React, { useState } from "react";
+import data from "./../data";
 
 function App() {
-  const [ numberOfParagraph, setNumberOfParagraph ] = useState(0);
-  const [ text, setText ] = useState([]);
-  
+  const [numberOfParagraph, setNumberOfParagraph] = useState(0);
+  const [text, setText] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-      setText(() => data.slice(0, numberOfParagraph));
-  }
-    
+    return setText(() => data.slice(0, parseInt(numberOfParagraph)));
+  };
+
   return (
-      <section className='section-center'>
-        <h3>tired of boring lorem ipsum ?</h3>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum ?</h3>
 
-        <form className='lorem-form' onSubmit={handleSubmit}>
-          <label htmlFor="numbParagraph">paragraph ?</label>
-          <input 
-            id="numbParagraph"
-            type="number" 
-            min="0"
-            max={data.length}
-            value={numberOfParagraph}
-            onChange={(event) => setNumberOfParagraph(event.target.value)} 
-          />
-          <button type="submit" className='btn'>Fetch</button> 
-        </form>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="numbParagraph">number of paragraph ?</label>
+        <input
+          id="numbParagraph"
+          type="number"
+          min="0"
+          max={data.length}
+          value={numberOfParagraph}
+          onChange={(event) => setNumberOfParagraph(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
 
-        <article className="lorem-text">
-          {text.map((item, index ) => {
-            return(
-              <p key={index}>{item}</p>
-            );
-          })}
-        </article>
-      </section>
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
   );
 }
 
