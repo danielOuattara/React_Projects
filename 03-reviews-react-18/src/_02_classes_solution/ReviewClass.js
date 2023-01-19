4794fbb97317587b123144be006e4aa617eb12c8
danielOuattara <daniel.ouattara@gmx.com>
2023-01-19 20:35:10 +0100

    classes solution

---
+++ 03-reviews-react-18/src/_02_classes_solution/ReviewClass.js
@@ -0,0 +1,75 @@
+import { Component } from "react";
+import people from "./../data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+export default class ReviewClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+    };
+  }
+
+  checkIndex = (argIndex) => {
+    if (argIndex > people.length - 1) {
+      return 0;
+    } else if (argIndex < 0) {
+      return people.length - 1;
+    } else {
+      return argIndex;
+    }
+  };
+
+  getPreviousQuote = () => {
+    this.setState((prevState) => {
+      return { index: this.checkIndex(prevState.index - 1) };
+    });
+  };
+
+  getNextQuote = () => {
+    this.setState((prevState) => {
+      return { index: this.checkIndex(prevState.index + 1) };
+    });
+  };
+
+  getRandomQuote = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === this.state.index) {
+      randomIndex = this.checkIndex(randomIndex - 1);
+    }
+    this.setState(() => {
+      return { index: randomIndex };
+    });
+  };
+
+  render() {
+    const { name, job, image, text } = people[this.state.index];
+    return (
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            {" "}
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="button-container">
+          <button className="prev-btn" onClick={this.getPreviousQuote}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={this.getNextQuote}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <div className="button-container">
+          <button className="random-btn" onClick={this.getRandomQuote}>
+            surprise me
+          </button>
+        </div>
+      </article>
+    );
+  }
+}
