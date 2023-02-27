7f97e0bce37243c1f349058bb7ca1a55d7fed218
danielOuattara <daniel.ouattara@gmx.com>
2023-02-27 19:24:49 +0100

    08 lorem ipsum OK

---
+++ 08-lorem-ipsum-react-18/src/_01_functions_solution/AppFunction.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import data from "./../data";
+
+function App() {
+  const [numberOfParagraph, setNumberOfParagraph] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    return setText(() => data.slice(0, parseInt(numberOfParagraph)));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum ?</h3>
+
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="numbParagraph">number of paragraph ?</label>
+        <input
+          id="numbParagraph"
+          type="number"
+          min="1"
+          max={data.length}
+          value={numberOfParagraph}
+          onChange={(event) => setNumberOfParagraph(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
+}
+
+export default App;
