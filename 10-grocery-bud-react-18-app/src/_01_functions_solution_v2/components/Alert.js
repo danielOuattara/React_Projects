27deb537003905ec08bed60a24b8a3c75b8e6b55
danielOuattara <daniel.ouattara@gmx.com>
2023-03-02 22:22:00 +0100

    func solution version2 : OK

---
+++ 10-grocery-bud-react-18-app/src/_01_functions_solution_v2/components/Alert.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+export default function Alert({ type, msg, showAlert }) {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      showAlert();
+    }, 1500);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+}
