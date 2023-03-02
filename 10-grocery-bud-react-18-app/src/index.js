5fb76c3eb6ee2a6e16bedbcfd1cc23fcb3702ce6
danielOuattara <daniel.ouattara@gmx.com>
2023-03-02 22:44:43 +0100

    update index.js for class solution

--- 10-grocery-bud-react-18-app/src/index.js
+++ 10-grocery-bud-react-18-app/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppFunction from "./_01_functions_solution/AppFunction";
+import AppFunctionV2 from "./_01_functions_solution_v2/AppFunctionV2";
 import AppClass from "./_02_classes_solution/AppClass";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppFunction />
+    <br /> <hr /> <br />
+    <AppFunctionV2 />
     <br /> <hr /> <br />
     <AppClass />
   </React.StrictMode>,
