7917067a413bcae60d6b18350459aab1644f05f5
danielOuattara <daniel.ouattara@gmx.com>
2023-03-13 14:12:45 +0100

    starting 11 navbar app with useRef

---
+++ 11-navbar-react-18-app/src/_02_classes_solution/AppClass.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+  render() {
+    return <div>AppClass</div>;
+  }
+}
---
+++ 11-navbar-react-18-app/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppFunction from "./_01_functions_solution/AppFunction";
+import AppClass from "./_02_classes_solution/AppClass";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <AppFunction />
+    <br /> <hr /> <br />
+    <AppClass />
+  </React.StrictMode>,
+);
