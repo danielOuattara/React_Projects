3c984508b59b5be18aa944dc7d0c709e04ac3f18
danielOuattara <daniel.ouattara@gmx.com>
2023-03-16 13:12:53 +0100

    00 github user fetch: classes solution v2

---
+++ 00-github-api-users-fetch-react-18-app/src/_02_classes_solution_v2/components/UserListItemClass.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+
+export default class UserListItemClass extends Component{
+  render() {
+    return (
+      <li>
+        <img src={this.props.person.avatar_url} alt={this.props.person.login} />
+        <div>
+          <h4>{this.props.person.login}</h4>
+          <a href={this.props.person.html_url} target="_blank" rel="noreferrer">
+            Link to {this.props.person.login}
+          </a>
+        </div>
+        <p>Hello</p>
+      </li>
+    );
+  }
+}
