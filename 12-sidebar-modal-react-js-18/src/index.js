5e32552d5ccee9f1d51b51314769f4768b4ffefb
danielOuattara <daniel.ouattara@gmx.com>
2023-03-16 12:13:50 +0100

    starting 12 sidebar app react -18

---
+++ 12-sidebar-modal-react-js-18/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppFunction from "./_01_functions_solution/AppFunction";
+import AppClass from "./_02_classes_solution/AppClass";
+import AppContextAPIFunction from "./_03_context_API_functions_solution/AppContextAPIFunction";
+import AppContextAPIClasses from "./_04_context_API_classes_solution/AppContextAPIClasses";
+import AppUseContextHookFunction from "./_05_useContext_hook_functions_solution/AppUseContextHookFunction";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <AppFunction />
+    <br /> <hr /> <br />
+    <AppClass />
+    <br /> <hr /> <br />
+    <AppContextAPIFunction />
+    <br /> <hr /> <br />
+    <AppContextAPIClasses />
+    <br /> <hr /> <br />
+    <AppUseContextHookFunction />
+    <br /> <hr /> <br />
+  </React.StrictMode>,
+);
