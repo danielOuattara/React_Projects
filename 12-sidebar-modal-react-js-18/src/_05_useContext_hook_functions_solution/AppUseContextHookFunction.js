617043d31726fc0c9e1a49106e6c0c0e7aeb73d4
danielOuattara <daniel.ouattara@gmx.com>
2023-03-16 12:16:00 +0100

    12 sidebar react-18: useContext function solution

---
+++ 12-sidebar-modal-react-js-18/src/_05_useContext_hook_functions_solution/AppUseContextHookFunction.js
@@ -0,0 +1,15 @@
+import Modal from "./components/Modal";
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import AppContextProvider from "./context/AppContext";
+
+export default function AppUseContextHookFunction() {
+  return (
+    <AppContextProvider>
+      <p style={{ textAlign: "center" }}>useContext hook function solution</p>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </AppContextProvider>
+  );
+}
