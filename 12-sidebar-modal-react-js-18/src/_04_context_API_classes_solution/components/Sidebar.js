853240ae0308c66a49656f11ae58ddc898d8c09a
danielOuattara <daniel.ouattara@gmx.com>
2023-03-16 19:08:48 +0100

    12 sidebar:  add context for contextAPI class

--- 12-sidebar-modal-react-js-18/src/_04_context_API_classes_solution/components/Sidebar.js
+++ 12-sidebar-modal-react-js-18/src/_04_context_API_classes_solution/components/Sidebar.js
@@ -2,43 +2,48 @@ import { Component } from "react";
 import logo from "./../logo.svg";
 import { FaTimes } from "react-icons/fa";
 import { social, links } from "../data";
+import { AppContext } from "../context/AppContext";
 
 export default class Sidebar extends Component {
   render() {
     return (
-      <aside
-        className={
-          this.props.isSideBarOpen ? "sidebar show-sidebar" : "sidebar"
-        }
-      >
-        <div className="sidebar-header">
-          <img src={logo} alt="coding addict" className="logo" />
-          <button
-            className="close-btn"
-            onClick={() => this.props.toggleSideBar(!this.props.isSideBarOpen)}
+      <AppContext.Consumer>
+        {(context) => (
+          <aside
+            className={
+              context.isSideBarOpen ? "sidebar show-sidebar" : "sidebar"
+            }
           >
-            <FaTimes />
-          </button>
-        </div>
-        <ul className="links">
-          {links.map((item) => (
-            <li key={item.id}>
-              {" "}
-              <a href={item.url}>
-                {item.icon}
-                {item.text}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <ul className="social-icons">
-          {social.map((item) => (
-            <li key={item.id}>
-              <a href={item.url}>{item.icon}</a>
-            </li>
-          ))}
-        </ul>
-      </aside>
+            <div className="sidebar-header">
+              <img src={logo} alt="coding addict" className="logo" />
+              <button
+                className="close-btn"
+                onClick={() => context.toggleSideBar(!context.isSideBarOpen)}
+              >
+                <FaTimes />
+              </button>
+            </div>
+            <ul className="links">
+              {links.map((item) => (
+                <li key={item.id}>
+                  {" "}
+                  <a href={item.url}>
+                    {item.icon}
+                    {item.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <ul className="social-icons">
+              {social.map((item) => (
+                <li key={item.id}>
+                  <a href={item.url}>{item.icon}</a>
+                </li>
+              ))}
+            </ul>
+          </aside>
+        )}
+      </AppContext.Consumer>
     );
   }
 }
