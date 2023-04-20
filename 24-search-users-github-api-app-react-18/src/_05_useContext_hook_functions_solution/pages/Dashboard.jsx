df8d3fb43a8af242ecdc8b6b9dcbca343c9adc16
danielOuattara <daniel.ouattara@gmx.com>
2023-04-21 01:36:46 +0200

    25- 05 added components to home = dashboard

--- 24-search-users-github-api-app-react-18/src/_05_useContext_hook_functions_solution/pages/Dashboard.jsx
+++ 24-search-users-github-api-app-react-18/src/_05_useContext_hook_functions_solution/pages/Dashboard.jsx
@@ -1,11 +1,15 @@
-import { Info, Repos, User, Search, Navbar } from "../components";
+import { Info, Repos, User, Search, Navbar } from "./../components";
 import loadingImage from "./../../images/preloader.gif";
 import { useGitHubContext } from "./../context";
 
 export default function Dashboard() {
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <Navbar />
+      <Search />
+      <Info />
+      <User />
+      <Repos />
     </main>
   );
 }
