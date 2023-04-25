5455874b979d5b6ff1373f6443d25eb034c3e390
danielOuattara <daniel.ouattara@gmx.com>
2023-04-25 15:57:51 +0200

    26:contentful CMS project. my solution completed

--- 26-contentful-cms-project-react-18/my_solution/src/components/Project.jsx
+++ 26-contentful-cms-project-react-18/my_solution/src/components/Project.jsx
@@ -1,3 +1,41 @@
+import useFetchProjects from "../useFetchProjects";
+
 export default function Project() {
-  return <div>Project</div>;
+  const { isLoading, projects } = useFetchProjects();
+
+  console.log(projects);
+
+  if (isLoading) {
+    return (
+      <section className="projects">
+        <p className="loading">...loading </p>;
+      </section>
+    );
+  }
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.items.map((item) => (
+          <a
+            href={item.fields.url}
+            className="project"
+            key={item.sys.id}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="img"
+              src={item.fields.image.fields.file.url}
+              alt={item.fields.title}
+            />
+            <h5 className="title">{item.fields.title}</h5>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
 }
---
+++ 26-contentful-cms-project-react-18/my_solution/src/useFetchProjects.jsx
@@ -0,0 +1,30 @@
+import { createClient } from "contentful";
+import { useState, useEffect } from "react";
+
+const client = createClient({
+  space: import.meta.env.VITE_API_CLIENT_SPACE,
+  environment: import.meta.env.VITE_API_CLIENT_ENVIRONMENT,
+  accessToken: import.meta.env.VITE_API_CLIENT_ACCESS_TOKEN,
+});
+
+export default function useFetchProjects() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await client.getEntries({
+          content_type: "projectsContentfulJohnSmilga",
+        });
+        setProjects(response);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProjects();
+  }, []);
+
+  return { isLoading, projects };
+}
