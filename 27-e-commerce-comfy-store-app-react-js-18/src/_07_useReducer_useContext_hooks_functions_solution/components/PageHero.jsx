5a8f207d69ad95b4978ef2c0a8546db2a12d65a3
danielOuattara <daniel.ouattara@gmx.com>
2023-05-02 18:46:54 +0200

    start up pages: checkout, hero, about

--- 27-e-commerce-comfy-store-app-react-js-18/src/_07_useReducer_useContext_hooks_functions_solution/components/PageHero.jsx
+++ 27-e-commerce-comfy-store-app-react-js-18/src/_07_useReducer_useContext_hooks_functions_solution/components/PageHero.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
 import { PageHeroWrapper } from "./styleWrappers";
 
-export default function PageHero() {
-  return <h4>page hero</h4>;
+export default function PageHero(props) {
+  return (
+    <PageHeroWrapper>
+      <div className="section-center">
+        <h3>
+          <Link to={"/"}>Home</Link> / {props.title}
+        </h3>
+      </div>
+    </PageHeroWrapper>
+  );
 }
--- 27-e-commerce-comfy-store-app-react-js-18/src/_07_useReducer_useContext_hooks_functions_solution/pages/CheckoutPage.jsx
+++ 27-e-commerce-comfy-store-app-react-js-18/src/_07_useReducer_useContext_hooks_functions_solution/pages/CheckoutPage.jsx
@@ -2,7 +2,15 @@ import { PageHero, StripeCheckout } from "../components";
 // extra imports
 import { useCartContext } from "../context";
 import { Link } from "react-router-dom";
+import { CheckoutPageWrapper } from "./styleWrappers";
 
 export default function CheckoutPage() {
-  return <h4>checkout page</h4>;
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <CheckoutPageWrapper className="page">
+        <h2>checkout</h2>
+      </CheckoutPageWrapper>
+    </main>
+  );
 }
