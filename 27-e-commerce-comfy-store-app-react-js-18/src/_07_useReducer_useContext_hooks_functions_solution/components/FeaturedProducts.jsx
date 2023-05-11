3228dc6fb99af56eaffed7933e63101e72e73f22
danielOuattara <daniel.ouattara@gmx.com>
2023-05-11 16:57:00 +0200

    featuredProduct slice(). Automatic random later

--- 27-e-commerce-comfy-store-app-react-js-18/src/_07_useReducer_useContext_hooks_functions_solution/components/FeaturedProducts.jsx
+++ 27-e-commerce-comfy-store-app-react-js-18/src/_07_useReducer_useContext_hooks_functions_solution/components/FeaturedProducts.jsx
@@ -1,50 +1,13 @@
 import { useProductsContext } from "../context/";
-import { Link } from "react-router-dom";
+import { Link, useFetcher } from "react-router-dom";
 import Error from "./Error";
 import Loading from "./Loading";
 import Product from "./Product";
 import { FeaturedProductsWrapper } from "./styleWrappers";
-import { featuredProductsRandomizer } from "../../utilities";
-import { useEffect, useState } from "react";
 
 export default function FeaturedProducts() {
   const { isProductsLoading, isProductsError, featuredProducts } =
     useProductsContext();
-
-  // const [featuredProductsRandomized, setFeaturedProductsRandomized] = useState(
-  //   featuredProducts.slice(0, 3),
-  // );
-
-  const featuredProductsMaxLength = 3;
-
-  const featuredProductsRandomized = featuredProductsRandomizer(
-    featuredProducts,
-    featuredProductsMaxLength,
-  );
-
-  //--------------------------------
-  useEffect(() => {
-    const timer = setInterval(() => {
-      featuredProductsRandomizer(featuredProducts, featuredProductsMaxLength);
-    }, [5000]);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [featuredProducts]);
-
-  //--------------------------------
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-  //     setFeaturedProductsRandomized(
-  //       featuredProductsRandomizer(featuredProducts, featuredProductsMaxLength),
-  //     );
-  //   }, [5000]);
-
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, [featuredProducts]);
 
   if (isProductsLoading) {
     return (
@@ -65,10 +28,114 @@ export default function FeaturedProducts() {
         <div className="underline"></div>
       </div>
       <div className="section-center featured">
-        {featuredProductsRandomized.map((product) => (
-          <Product key={product.id} {...product} />
-        ))}
+        {featuredProducts /* .slice(0, 3) */
+          .map((product) => (
+            <Product key={product.id} {...product} />
+          ))}
       </div>
     </FeaturedProductsWrapper>
   );
 }
+
+//-------------------------------------------------------------------
+
+// import { useProductsContext } from "../context/";
+// import { Link, useFetcher } from "react-router-dom";
+// import Error from "./Error";
+// import Loading from "./Loading";
+// import Product from "./Product";
+// import { FeaturedProductsWrapper } from "./styleWrappers";
+// import {
+//   featuredProductsRandomizer,
+//   randomArrayIndexer,
+// } from "../../utilities";
+// import { useEffect, useState } from "react";
+
+// export default function FeaturedProducts() {
+//   const { isProductsLoading, isProductsError, featuredProducts } =
+//     useProductsContext();
+
+//   const initialFeaturedProducts = featuredProducts.slice(0, 3);
+//   const [featuredProductsRandomized, setFeaturedProductsRandomized] = useState(
+//     initialFeaturedProducts,
+//   );
+
+//   // const [featuredProductsRandomized, setFeaturedProductsRandomized] = useState(
+//   //   () => featuredProducts.slice(0, 3),
+//   // );
+
+//   const maxFeaturedProducts = 3;
+
+//   //--------------------------------
+//   // useEffect(() => {
+//   //   let featuredProductsRandomized = featuredProductsRandomizer(
+//   //     featuredProducts,
+//   //     featuredProductsMaxLength,
+//   //   );
+//   //   const timer = setInterval(() => {
+//   //     const arrayOfRandomIndexes = randomArrayIndexer(
+//   //       featuredProductsMaxLength,
+//   //       featuredProducts.length,
+//   //     );
+//   //     // featuredProductsRandomizer(featuredProducts, featuredProductsMaxLength);
+//   //     featuredProductsRandomized = featuredProductsRandomized.filter(
+//   //       (item, index) => {
+//   //         return arrayOfRandomIndexes.forEach((element, elementIndex) => {
+//   //           return index === elementIndex;
+//   //         });
+//   //       },
+//   //     );
+//   //   }, [5000]);
+
+//   //   return () => {
+//   //     clearInterval(timer);
+//   //   };
+//   // }, [featuredProducts]);
+
+//   //--------------------------------
+
+//   // useEffect(() => {
+//   //   setFeaturedProductsRandomized(() => featuredProducts.slice(0, 3));
+//   // }, []);
+//   // useEffect(() => {
+//   //   const timer = setInterval(() => {
+//   //     setFeaturedProductsRandomized(() =>
+//   //       featuredProductsRandomizer(featuredProducts, maxFeaturedProducts),
+//   //     );
+//   //   }, [5000]);
+
+//   //   return () => {
+//   //     clearInterval(timer);
+//   //   };
+//   // }, [featuredProducts]);
+
+//   if (isProductsLoading) {
+//     return (
+//       <div className="section section-center">
+//         <Loading />
+//       </div>
+//     );
+//   }
+
+//   if (isProductsError) {
+//     return <Error />;
+//   }
+
+//   return (
+//     <FeaturedProductsWrapper className="section">
+//       <div className="title">
+//         <h2> featured products</h2>
+//         <div className="underline"></div>
+//       </div>
+//       <div className="section-center featured">
+//         {(featuredProductsRandomized || initialFeaturedProducts).map(
+//           (product) => (
+//             <Product key={product.id} {...product} />
+//           ),
+//         )}
+//       </div>
+//     </FeaturedProductsWrapper>
+//   );
+// }
+
+//----------------------------------------------------------------------------------------
