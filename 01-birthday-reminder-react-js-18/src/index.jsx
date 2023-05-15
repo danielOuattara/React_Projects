6ecad6ae7d5eeaa31122d1d5796b03228cf44bf5
danielOuattara <daniel.ouattara@gmx.com>
2023-05-15 02:15:33 +0200

    project global update

---
+++ 01-birthday-reminder-react-js-18/src/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppFunction from "./_01_functions_solution/AppFunction";
+import AppClass from "./_02_classes_solution/AppClass";
+import AppFunctionContextAPI from "./_03_contextAPI_functions_solution/AppFunctionContextAPI";
+import AppClassContextAPI from "./_04_contextAPI_classes_solution/AppClassContextAPI";
+import AppFunctionContextAPIUseContextHook from "./_05_contextAPI_useContext_hook_functions_solution/AppFunctionContextAPIUseContextHook";
+import AppFunctionUseReducer from "./_06_useReducer_hook_functions_solution/AppFunctionUseReducer";
+import AppFunctionUseReducerContextAPI from "./_07_useReducer_contextAPI_functions_solution /AppFunctionUseReducerContextAPI";
+import AppFunctionUseReducerUseContext from "./_08_useReducer_useContext_hooks_functions_solution/AppFunctionUseReducerUseContext";
+import AppFunctionRedux from "./_09_react_redux_functions_solution/AppFunctionRedux";
+import AppClassRedux from "./_10_react_redux_classes_solution/AppClassRedux";
+import AppFunctionReduxToolkit from "./_11_redux_toolkit_functions_solution/AppFunctionReduxToolkit";
+import AppClassReduxToolkit from "./12_redux_toolkit_classes_solution/AppClassReduxToolkit";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppFunction />
+    <hr />
+    <AppClass />
+    <hr />
+    <AppFunctionContextAPI />
+    <hr />
+    <AppClassContextAPI />
+    <hr />
+    <AppFunctionContextAPIUseContextHook />
+    <hr />
+    <AppFunctionUseReducer />
+    <hr />
+    <AppFunctionUseReducerContextAPI />
+    <hr />
+    <AppFunctionUseReducerUseContext />
+    <hr />
+    <AppFunctionRedux />
+    <hr />
+    <AppClassRedux />
+    <hr />
+    <AppFunctionReduxToolkit />
+    <hr />
+    <AppClassReduxToolkit />
+    <hr />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
