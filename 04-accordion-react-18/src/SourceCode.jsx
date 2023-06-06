a97fd7b64b75914774186a3d7c73d66bd1e8877b
danielOuattara <daniel.ouattara@gmx.com>
2023-06-06 22:51:27 +0200

    04 accordion: add link to gitHub code

---
+++ 04-accordion-react-18/src/SourceCode.jsx
@@ -0,0 +1,11 @@
+export default function SourceCode() {
+  return (
+    <section className="container">
+      <p>
+        <a href="https://github.com/danielOuattara/React_Projects/tree/master/04-accordion-react-18">
+          Link: to gitHub source code
+        </a>{" "}
+      </p>
+    </section>
+  );
+}
