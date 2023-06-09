b7365d55f953428197297745fa828ef85cbe7e51
danielOuattara <daniel.ouattara@gmx.com>
2023-06-09 23:55:34 +0200

    09 color gen: 05 context API + useContext  solut°

---
+++ 09-react-18-color-generator-app/src/_05_contextAPI_useContext_hook_functions_solution/components/Container.jsx
@@ -0,0 +1,11 @@
+import { ColorForm, ColorList } from "./";
+
+export default function Container() {
+  return (
+    <>
+      <p className="title">context API + useContext hooks solutions</p>
+      <ColorForm />
+      <ColorList />
+    </>
+  );
+}
