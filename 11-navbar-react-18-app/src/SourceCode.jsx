7d07ee80ec11f4d07d57929f662d2dce0d896469
danielOuattara <daniel.ouattara@gmx.com>
2023-06-14 15:23:03 +0200

    11 navbar: root files

---
+++ 11-navbar-react-18-app/src/SourceCode.jsx
@@ -0,0 +1,22 @@
+export default function SourceCode() {
+  return (
+    <p
+      style={{
+        background: "#e7dada",
+        width: "50%",
+        padding: "2rem",
+        margin: "auto",
+        marginTop: "2rem",
+        marginBottom: "4rem",
+      }}
+    >
+      <a
+        href="https://github.com/danielOuattara/React_Projects/tree/master/11-navbar-react-18-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Link: to gitHub source code
+      </a>{" "}
+    </p>
+  );
+}
