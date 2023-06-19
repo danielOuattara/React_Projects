8f086f3c23b3ce3e6eee57c35c07d084b7d5cf5d
danielOuattara <daniel.ouattara@gmx.com>
2023-06-19 18:07:09 +0200

    14 cart: 08 useReducer+ useContext solution V1

---
+++ 14-cart-app-react-js-18/src/_08_useReducer_useContext_hooks_functions_solution_v1/context/CartContext.jsx
@@ -0,0 +1,74 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+
+import cartItemsReducer from "./../reducer/cartItems/cartItemsReducer";
+import {
+  clearCart,
+  removeItem,
+  updateQuantity,
+  loading,
+  loadCartData,
+  getTotals,
+} from "./../reducer/cartItems/cartItemsActions";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+const CartContext = createContext();
+
+const initialCartItemsState = {
+  isLoading: false,
+  cart: [],
+  totalPrice: 0,
+  totalItems: 0,
+};
+
+export default function CartContextProvider(props) {
+  const [cartItemsState, dispatchCartItems] = useReducer(
+    cartItemsReducer,
+    initialCartItemsState,
+  );
+
+  const handleClearCart = () => {
+    return dispatchCartItems(clearCart());
+  };
+
+  const handleRemoveItem = (id) => {
+    return dispatchCartItems(removeItem(id));
+  };
+
+  const handleUpdateQuantity = (id, value) => {
+    return dispatchCartItems(updateQuantity(id, value));
+  };
+
+  const handleFetchCartItems = async () => {
+    dispatchCartItems(loading());
+    const res = await fetch(url);
+    const fetchedCart = await res.json();
+
+    dispatchCartItems(loadCartData(fetchedCart));
+  };
+
+  useEffect(() => {
+    handleFetchCartItems();
+  }, []);
+
+  useEffect(() => {
+    dispatchCartItems(getTotals());
+  }, [cartItemsState.cart]);
+
+  const context = {
+    ...cartItemsState,
+    clearCart: handleClearCart,
+    removeItem: handleRemoveItem,
+    updateQuantity: handleUpdateQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={context}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
+
+export const useCartContext = () => {
+  return useContext(CartContext);
+};
