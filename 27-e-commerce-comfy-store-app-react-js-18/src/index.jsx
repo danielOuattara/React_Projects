63b4647255394d46989c4dc2f8ca880c944b3893
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 22:28:36 +0200

    27E-commerce: adding gitHub source and title

--- 27-e-commerce-comfy-store-app-react-js-18/src/index.jsx
+++ 27-e-commerce-comfy-store-app-react-js-18/src/index.jsx
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import AppUseReducerUseContextHooksFunctions from "./_08_useReducer_useContext_hooks_functions_solution/AppUseReducerUseContextHooksFunctions";
+import SourceCode from "./SourceCode";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
+    <SourceCode />
+    <hr />
     <AppUseReducerUseContextHooksFunctions />
   </>,
 );
