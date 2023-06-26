873be2ce9a61d01523341ad50a492d0fe973111f
danielOuattara <daniel.ouattara@gmx.com>
2023-06-26 22:43:19 +0200

    15 cocktails : adding gitHub SourceCode

---
+++ 15-cocktails-app-react-18/src/SourceCode.jsx
@@ -0,0 +1,22 @@
+export default function SourceCode() {
+  return (
+    <p
+      style={{
+        background: "#e7dada",
+        width: "50%",
+        padding: "2rem",
+        margin: "auto",
+        marginTop: "2rem",
+        marginBottom: "4rem",
+      }}
+    >
+      <a
+        href="https://github.com/danielOuattara/React_Projects/tree/master/15-cocktails-app-react-18"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Link: to gitHub source code
+      </a>{" "}
+    </p>
+  );
+}
