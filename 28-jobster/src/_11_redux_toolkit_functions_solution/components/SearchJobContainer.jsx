3dcaa77a22c781a1ea8159c34916bbd489bd0be2
danielOuattara <daniel.ouattara@gmx.com>
2023-07-06 20:32:27 +0200

    28 jobster: debounce setup.Course ended 74.5hours

--- 28-jobster/src/_11_redux_toolkit_functions_solution/components/SearchJobContainer.jsx
+++ 28-jobster/src/_11_redux_toolkit_functions_solution/components/SearchJobContainer.jsx
@@ -2,13 +2,16 @@ import { FormInputRow, FormSelectRow } from "./index";
 import { SearchContainerWrapper } from "../../assets/styles";
 import { useSelector, useDispatch } from "react-redux";
 import { allJobsAction } from "../redux/allJobs/allJobsSlice";
+import { useState, useMemo } from "react";
 
 export default function SearchJobContainer() {
+  const [localSearch, setLocalSearch] = useState("");
+
   const dispatch = useDispatch();
 
   const {
     isLoadingAllJobs,
-    search,
+    // search,
     searchStatus,
     searchType,
     sort,
@@ -29,6 +32,30 @@ export default function SearchJobContainer() {
     );
   };
 
+  const debounce = () => {
+    let timeoutID;
+    return (event) => {
+      clearTimeout(timeoutID);
+      setLocalSearch(event.target.value);
+      timeoutID = setTimeout(() => {
+        dispatch(
+          allJobsAction.handleChangeFilters({
+            name: event.target.name,
+            value: event.target.value,
+          }),
+        );
+      }, 1000);
+    };
+  };
+
+  const handleClearInput = () => {
+    setLocalSearch("");
+    dispatch(allJobsAction.clearSearchFilters());
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const optimizedDebounce = useMemo(() => debounce(), []);
+
   return (
     <SearchContainerWrapper>
       <form className="form">
@@ -38,8 +65,9 @@ export default function SearchJobContainer() {
             // search position
             type="text"
             name="search"
-            handleChange={handleSearch}
-            value={search}
+            // handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
 
           <FormSelectRow
@@ -69,7 +97,7 @@ export default function SearchJobContainer() {
             className="btn btn-block btn-danger"
             disabled={isLoadingAllJobs}
             type="button"
-            onClick={() => dispatch(allJobsAction.clearSearchFilters())}
+            onClick={handleClearInput}
           >
             {" "}
             clear filters
