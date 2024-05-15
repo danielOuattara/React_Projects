877f5672ac37fd47ca6aa7e16b3bcb9113dcf4ef
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-15 20:08:01 +0200

    31 Web Dev portfolio TailWindCSS: Navbar component

--- 31-WebDev-portfolio-TailwindCSS/src/App.jsx
+++ 31-WebDev-portfolio-TailwindCSS/src/App.jsx
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
-  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
---
+++ 31-WebDev-portfolio-TailwindCSS/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { links } from "../data";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-emerald-100">
+      <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
+        <h2 className="text-3xl font-bold">
+          Web
+          <span className="text-emerald-600">Dev</span>
+        </h2>
+        <div className="flex gap-x-3">
+          {links.map((link) => (
+            <a
+              key={link.id}
+              href={link.href}
+              className="capitalize text-lg tracking-wide hover:text-emerald-600 duration-300"
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+}
