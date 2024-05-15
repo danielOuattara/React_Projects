56338bebf6942a7b13cfcbcbdbf5c48d6ff7ef79
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-15 17:26:01 +0200

    30 mix-master :HomeLayout updated

--- 30-mix-master-drinks/src/_01_functions_solution/layout/HomeLayout.jsx
+++ 30-mix-master-drinks/src/_01_functions_solution/layout/HomeLayout.jsx
@@ -3,32 +3,20 @@ import { Navbar } from "../components";
 
 export default function HomeLayout() {
   const navigation = useNavigation();
-  // const data = "some data"; // for info purpose only, check the docs for more
-
-  //---------------------------
-  // return (
-  //   <>
-  //     <Navbar />
-  //     <section className="page">
-  //       {navigation.state === "loading" ? (
-  //         <div className="loading" />
-  //       ) : (
-  //         <Outlet
-  //         // for info purpose only, check the docs for more
-  //         // context={{ data }}
-  //         />
-  //       )}
-  //     </section>
-  //   </>
-  // );
-
-  //---------------------------
+  const data = "some data"; // for info purpose only, check the docs for more
 
   return (
     <>
       <Navbar />
       <section className="page">
-        <Outlet />
+        {navigation.state === "loading" ? (
+          <div className="loading" />
+        ) : (
+          <Outlet
+            // for info purpose only, check the docs for more
+            context={{ data }}
+          />
+        )}
       </section>
     </>
   );
@@ -37,3 +25,23 @@ export default function HomeLayout() {
 export async function loader({ request }) {
   return "something from homeLayout";
 }
+
+//--------------------------------------------------------
+
+// import { Outlet } from "react-router-dom";
+// import { Navbar } from "../components";
+
+// export default function HomeLayout() {
+//   return (
+//     <>
+//       <Navbar />
+//       <section className="page">
+//         <Outlet />
+//       </section>
+//     </>
+//   );
+// }
+
+// export async function loader({ request }) {
+//   return "something from homeLayout";
+// }
