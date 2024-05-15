5c7065177d0d21ad6a5eec8a2bdd4bb246a22dad
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-15 20:46:59 +0200

    31 Web Dev portfolio TailWindCSS: Reduce style

--- 31-WebDev-portfolio-TailwindCSS/src/components/Hero.jsx
+++ 31-WebDev-portfolio-TailwindCSS/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 export default function Hero() {
   return (
     <section className="bg-emerald-100 py-24  ">
-      <div className=" mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+      <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
           <h1 className="text-7xl font-bold tracking-wider">
             I&apos;m John Doe
