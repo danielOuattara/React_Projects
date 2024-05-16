51b350c60b2dc64ae21fb67cb845c0b9e5e27c32
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-16 13:25:53 +0200

    32 comfy store: Typescript: React Router Routes structure

--- 32-Comfy_Store_React-router_RTK_Axios_React-Query/typescript-comfy_Store_React-router_RTK_Axios_React-Query/src/App.tsx
+++ 32-Comfy_Store_React-router_RTK_Axios_React-Query/typescript-comfy_Store_React-router_RTK_Axios_React-Query/src/App.tsx
@@ -1,4 +1,33 @@
 import * as Page from "./pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Page.HomeLayout />,
+    errorElement: <Page.Error />,
+    children: [
+      { index: true, element: <Page.Landing /> },
+      { path: "/products", element: <Page.Products /> },
+      { path: "/products/:productId", element: <Page.SingleProduct /> },
+      { path: "/cart", element: <Page.Cart /> },
+      { path: "/about", element: <Page.About /> },
+      { path: "/checkout", element: <Page.Checkout /> },
+      { path: "/orders", element: <Page.Orders /> },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Page.Register />,
+    errorElement: <Page.Error />,
+  },
+  {
+    path: "/register",
+    element: <Page.Register />,
+    errorElement: <Page.Error />,
+  },
+]);
+
 export default function App() {
-  return <div className="text-2xl">App</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
