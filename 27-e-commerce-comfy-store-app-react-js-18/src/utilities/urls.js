654f451161cb08f425d494a646893defed0105e5
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-01 18:15:21 +0200

    27 e-commerce-comfy-store: update url for products

--- 27-e-commerce-comfy-store-app-react-js-18/src/utilities/urls.js
+++ 27-e-commerce-comfy-store-app-react-js-18/src/utilities/urls.js
@@ -1,5 +1,5 @@
-const products_url = "https://course-api.com/react-store-products";
+const products_url = "https://www.course-api.com/react-store-products";
 
-const single_product_url = `https://course-api.com/react-store-single-product?id=`;
+const single_product_url = `https://www.course-api.com/react-store-single-product?id=`;
 
 export { products_url, single_product_url };
